61681ca99c23c9c161181f9648b84ab3b77bf042
Miguel <animatorpizzaguy74@gmail.com>
2024-05-23 00:16:33 +0800

    Added recipe files
    Includes blueprint

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form, StringField, PasswordField, FileField, TextAreaField, IntegerField, SelectField, DecimalField
+from wtforms.validators import Email, DataRequired, Length, NumberRange
+from validators import unique_data, password_complexity, data_exist, otp_validator, six_digit_postal_code_validator, phone_number_validator, card_number_validator, card_expiry_validator
+
+class CreateRecipeForm(Form):
+    name = StringField("Name", validators=[DataRequired(), Length(max=255)], render_kw={"class": "form-control"})
+    ingredients = StringField("Ingredients", validators=[DataRequired()], render_kw={"class": "form-control"})
+    instructions = TextAreaField("Instructions", validators=[DataRequired()], render_kw={"class": "form-control"})
+    picture = FileField("Picture", render_kw={"class": "form-control", "accept": "image/*"})
---
+++ app/recipe.py
@@ -0,0 +1,38 @@
+class Recipe:
+    count_id = 0
+
+    def __init__(self, name, ingredients, instructions, picture):
+        Recipe.count_id += 1
+        self.__id = str(Recipe.count_id)
+        self.__name = name
+        self.__ingredients = ingredients
+        self.__instructions = instructions
+        self.__picture = picture
+
+    def get_id(self):
+        return self.__id
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def set_ingredients(self, ingredients):
+        self.__ingredients = ingredients
+
+    def get_ingredients(self):
+        return self.__ingredients
+
+    def set_instructions(self, instructions):
+        self.__instructions = instructions
+
+    def get_instructions(self):
+        return self.__instructions
+
+    def get_picture(self):
+        return self.__picture
+
+    def set_picture(self, picture):
+        self.__picture = picture
+
