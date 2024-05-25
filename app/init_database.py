d8a53f63b7a2e8c2be4528497710a67db53b5913
Miguel <animatorpizzaguy74@gmail.com>
2024-05-25 22:21:16 +0800

    Changed my files

---
+++ app/init_database.py
@@ -0,0 +1,18 @@
+import pymysql
+
+"""
+This is for initialising all the databases in the MySQL server
+To be executed when mySQL server starts
+"""
+
+mydb = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='password123'
+)
+
+my_cursor = mydb.cursor()
+
+
+
+
