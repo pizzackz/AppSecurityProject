0382da82a213fe8590d0babe59053387b821d89f
Unknown2348 <ongzhaohan03@gmail.com>
2024-05-25 15:41:57 +0800

    Refactored multiple files to be more organised
    Renamed 'blueprints.admin_dashboard.py' to 'admin_dashboard_bp.py', moved it to blueprints/.
    
    Created 2 new folders of the same name 'recipe' under static/css and static/js.
    
    Updated files 'recipe_database.html', 'recipe_update.html', 'recipe_view' and 'recipe_create.html' to use correct import statements for their respective css and javascript files

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from wtforms import Form, StringField, PasswordField, FileField, TextAreaField, 
 from wtforms.validators import Email, DataRequired, Length, NumberRange
 from validators import unique_data, password_complexity, data_exist, otp_validator, six_digit_postal_code_validator, phone_number_validator, card_number_validator, card_expiry_validator
 
+
 class CreateRecipeForm(Form):
     name = StringField("Name", validators=[DataRequired(), Length(max=255)], render_kw={"class": "form-control"})
     ingredients = StringField("Ingredients", validators=[DataRequired()], render_kw={"class": "form-control"})
