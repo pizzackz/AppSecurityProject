9a0a2b227d78a131e471b0aba606544d0a072497
Unknown2348 <ongzhaohan03@gmail.com>
2024-05-28 18:43:29 +0800

    Created forms, updated, models, created form validators, updated configurations

---
+++ app/form_validators.py
@@ -0,0 +1,78 @@
+import re
+import bleach
+from wtforms import ValidationError
+from app.models import User
+
+
+# Function to sanitise data first before validating
+def sanitise_data(data: str) -> str:
+    sanitised_data = bleach.clean(data)
+    return sanitised_data
+
+
+# Validtor for unique username
+def unique_username(form, field):
+    if User.query.filter_by(username=sanitise_data(field.data)).first():
+        raise ValidationError("Username is already in use. Please choose another one.")
+    
+
+# Validator for unique email
+def unique_email(form, field):
+    if User.query.filter_by(email=sanitise_data(field.data)).first():
+        raise ValidationError("Email is already in use. Please choose another one.")
+
+
+# Validator for OTP length
+def validate_otp(form, field):
+    if not sanitise_data(field.data).isdigit() or len(sanitise_data(field.data)) != 6:
+        raise ValidationError("OTP must be a 6 digit number.")
+
+
+# Validators for password complextiy policy
+def validate_password_complexity(form, field):
+    sanitised_data: str = sanitise_data(field.data)
+    errors: list[int] = list()
+
+    errors.append(validate_password_length(form, sanitise_data(field.data)))
+    errors.append(validate_password_upper(form, sanitise_data(field.data)))
+    errors.append(validate_password_lower(form, sanitise_data(field.data)))
+    errors.append(validate_password_symbol(form, sanitise_data(field.data)))
+
+    if len(errors) > 0:
+        raise ValidationError("\n".join(errors))
+
+
+# Validate password length
+def validate_password_length(form, field):
+    if len(field.data) < 8:
+        return "Password must be at least 8 characters long."
+
+
+# Validate password at least 1 uppercase
+def validate_password_upper(form, field):
+    if not re.search(r"[A-Z]", field.data):
+        return "Password must contain at least 1 uppercase letter."
+
+
+# Validate password at least 1 lowercase
+def validate_password_lower(form, field):
+    if not re.search(r"[A-Z]", field.data):
+        return "Password must contain at least 1 uppercase letter."
+
+
+# Validate password at least 1 symbol
+def validate_password_symbol(form, field):
+    if not re.search(r"\W", field.data):
+        return "Password must contain at least 1 symbol."
+
+
+# Validate phone number
+def validate_phone_number(form, field):
+    if not sanitise_data(field.data).isdigit() or not (10 <= len(sanitise_data(field.data)) <= 15):
+        raise ValidationError("Phone number must only contain numbers and be between 10 and 15 digits long.")
+
+
+# Validate postal code
+def validate_postal_code(form, field):
+    if not field.data.isdigit() or not (3 <= len(field.data) <= 10):
+        raise ValidationError("Postal code must only contain numbers and be between 3 and 10 digits long.")
--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,41 @@
-from wtforms import Form, StringField, PasswordField, FileField, TextAreaField, IntegerField, SelectField, DecimalField, SubmitField, validators
-from wtforms.validators import Email, DataRequired, Length, NumberRange
-# from validators import unique_data, password_complexity, data_exist, otp_validator, six_digit_postal_code_validator, phone_number_validator, card_number_validator, card_expiry_validator
+from wtforms import Form, StringField, PasswordField, FileField, TextAreaField, IntegerField, SelectField, DecimalField, SubmitField
+from wtforms.validators import Email, DataRequired, Length, NumberRange, Optional, EqualTo
 from flask_wtf import FlaskForm
 
+from app.form_validators import unique_email, unique_username, validate_otp, validate_password_complexity, validate_phone_number, validate_postal_code
 
+
+# Signup related forms to accomodate different phases of signing up
+# Manual Signup phase 1 form, requires username & email
+class InitialSignupForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20), unique_username])
+    email = StringField("Email", validators=[DataRequired(), Email(), unique_email])
+    submit = SubmitField("Next")
+
+
+# Manual Signup phase 2 - Verify email using otp
+class VerifyOtpForm(FlaskForm):
+    otp = StringField("One Time Code", validators=[DataRequired(), Length(min=6, max=6), validate_otp])
+    submit = SubmitField("Verify")
+
+
+# Manual Signup & 1st time google signin - Set password
+class SetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6), validate_password_complexity])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Signup")
+
+
+# Signup optional fields - Save phone & address
+class PhoneAddressForm(FlaskForm):
+    phone_number = StringField("Phone Number", validators=[Optional(), Length(min=10, max=15), validate_phone_number])
+    address = StringField("Address", validators=[Optional(), Length(max=255)])
+    postal_code = StringField("Postal Code", validators=[Optional(), Length(max=20), validate_postal_code])
+    submit = SubmitField("Complete")
+    skip = SubmitField("Skip")
+
+
+# Create recipe form for members and admin, can double as Update recipe form
 class CreateRecipeForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(), Length(max=20)], render_kw={"class": "form-control"})
     ingredients = StringField("Ingredients", validators=[DataRequired()], render_kw={"class": "form-control"})
