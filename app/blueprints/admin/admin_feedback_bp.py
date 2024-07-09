dc8b2b2512040044cff68854f221a490f82119d2
jairuuss <rayfeer8@gmail.com>
2024-07-09 16:44:53 +0800

    feedback stuff

--- app/blueprints/admin/admin_feedback_bp.py
+++ app/blueprints/admin/admin_feedback_bp.py
@@ -9,10 +9,13 @@ from flask import (
     flash,
     url_for,
 )
+from app.models import Feedback
+from app import db
 
 admin_feedback_bp = Blueprint("admin_feedback_bp", __name__)
 
 @admin_feedback_bp.route("/admin/feedback")
 def admin_feedback():
-# err what the sigma
-    return render_template
+    feedback = Feedback.query.all()
+    print(feedback)
+    return render_template("admin/feedback/admin_feedback.html", feedback=feedback)
